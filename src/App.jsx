aa54d5be129b836e42f7b1930dfcb99d80dfa1fd
Amanda Li <a383li@uwaterloo.ca>
2023-11-02 21:51:52 -0400

    resolved stashed commits

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 <<<<<<< Updated upstream
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
-=======
-import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
->>>>>>> Stashed changes
 
 const App = () => {
   return (
     <BrowserRouter>
-<<<<<<< Updated upstream
       <div className='relative z-0 bg-primary'>
         <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
           <Navbar />
@@ -27,24 +23,6 @@ const App = () => {
       </div>
     </BrowserRouter>
   );
-=======
-    <div className="relative z-0 bg-primary">
-      <div className="bf-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar />
-        <Hero />
-      </div>
-      <About />
-      <Experience />
-      <Tech />
-      <Works />
-      <div className="relative z-0">
-        <Contact />
-        <StarsCanvas />
-      </div>
-    </div>
-    </BrowserRouter>
-  )
->>>>>>> Stashed changes
-}
 
+  }
 export default App;
